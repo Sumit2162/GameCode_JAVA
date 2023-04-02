1627ad54d0637fdc44d68b9af594f2ea46ca3592
Sumit Tomar <118624032+Sumit2162@users.noreply.github.com>
2023-04-02 18:49:14 +0530

    Update Player.java

--- Game_Codes/src/com/brainmentors/game/sprites/Player.java
+++ Game_Codes/src/com/brainmentors/game/sprites/Player.java
@@ -18,7 +18,6 @@ public abstract class Player implements GameConstants{
         protected int action ;
         protected boolean isJumping;
         BufferedImage fullImage;
-//        BufferedImage smallImage;
         BufferedImage defaultMove[]= new BufferedImage[TOTAL_MOVES];
         BufferedImage kick[]= new BufferedImage[TOTAL_MOVES];
         BufferedImage currentMoves[]= new BufferedImage[TOTAL_MOVES];
@@ -29,13 +28,11 @@ public abstract class Player implements GameConstants{
         	power = MAX_POWER;
         	isJumping=false;
         	x=200;
-//        	this.x=x;
         	h=w=190;
         	y=FLOOR-310;
         	speed = SPEED;
         	action =DEFAULT_MOVE;
          fullImage = ImageIO.read(Player.class.getResource(imageName));
-//         smallImage = fullImage.getSubimage(1350, 1135, 82, 120);
          isAttacking=false;
          defaultAction();
          kickAction();
@@ -107,27 +104,7 @@ public abstract class Player implements GameConstants{
 		public void setAction(int action) {
         	this.action = action;
         }
-//        private void kickAction() {
-//        	kick[0] = fullImage.getSubimage(0, 1492, 79, 116);
-//        	kick[1] = fullImage.getSubimage(78, 1492, 71, 114);
-//        	kick[2] = fullImage.getSubimage(149, 1491, 103, 120);
-//        	kick[3] = fullImage.getSubimage(251, 1491, 108, 120);
-//        	kick[4] = fullImage.getSubimage(358, 1492, 68, 119);
-//        	kick[5] = fullImage.getSubimage(425, 1491, 68, 118);
-//        	kick[6] = fullImage.getSubimage(492, 1489, 79, 120);
-//        	kick[7] = fullImage.getSubimage(62, 1136, 67, 125);
-//        }
-//        private void defaultAction() {
-//        	defaultMove[0] = fullImage.getSubimage(1350, 1135, 82, 120);
-//        	defaultMove[1] = fullImage.getSubimage(1430, 1135, 81, 120);
-//        	defaultMove[2] = fullImage.getSubimage(1510, 1134, 96, 121);
-//        	defaultMove[3] = fullImage.getSubimage(1604, 1135, 87, 119);
-//        	defaultMove[4] = fullImage.getSubimage(1690, 1134, 95, 123);
-//        	defaultMove[5] = fullImage.getSubimage(1784, 1134, 69, 122);
-//        	defaultMove[6] = fullImage.getSubimage(1852, 1133, 69, 123);
-//        	defaultMove[7] = fullImage.getSubimage(1920, 1133, 69, 125);
-//
-//        }
+
         int index =0;
         protected void printCurrentMove(Graphics g) {
         	if(index>7) {
